6df23b57dbd453f1c599627e547e15cc3a90dfbf
Yarden Eitan <yar@google.com>
2018-06-11 13:43:15 -0400

    update

--- Sources/ProjectAnalysis.swift
+++ Sources/ProjectAnalysis.swift
@@ -58,14 +58,14 @@ class ProjectAnalysis {
       LogFile.error("No project name or github url")
       return
     }
-    // Example of regex match: "2018-06-05 - 2018-06-18"
+    // Example of regex match: "2018-06-05 - 2018-06-18".
     let regex = "^[\\d]{4}-[\\d]{2}-[\\d]{2} - [\\d]{4}-[\\d]{2}-[\\d]{2}$"
     guard projectName.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil,
     let endDate = projectName.components(separatedBy: " - ").last else {
       LogFile.info("The project closed didn't fit the regex")
       return
     }
-    // Create a new project
+    // Create a new project.
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd"
     if let lastSprintEndDate = formatter.date(from: endDate),
@@ -85,27 +85,37 @@ class ProjectAnalysis {
       let lastBacklogID = githubAPI.createProjectColumn(name: "Last sprint backlog",
                                                         projectID: projectID)
 
-      // Create automation instruction cards
+      // Create automation instruction cards.
       if let backlogID = backlogID {
-        githubAPI.createProjectCard(cardsURL: DefaultConfigParams.githubBaseURL + "/projects/columns/" + backlogID + "/cards",
+        githubAPI.createProjectCard(cardsURL:
+          DefaultConfigParams.githubBaseURL + "/projects/columns/" + backlogID + "/cards",
                                     contentID: nil,
                                     contentType: nil,
-                                    note: "Please manage automation for the backlog. Change preset to \"To do\" and turn on \"Move all reopened issues here\", \"Move all reopened pull requests here\"")
+                                    note:
+          "Please manage automation for the backlog. Change preset to \"To do\" and turn on" +
+          "\"Move all reopened issues here\", \"Move all reopened pull requests here\"")
       }
       if let inProgressID = inProgressID {
-        githubAPI.createProjectCard(cardsURL: DefaultConfigParams.githubBaseURL + "/projects/columns/" + inProgressID + "/cards",
+        githubAPI.createProjectCard(cardsURL:
+          DefaultConfigParams.githubBaseURL + "/projects/columns/" + inProgressID + "/cards",
                                     contentID: nil,
                                     contentType: nil,
-                                    note: "Please manage automation for the backlog. Change preset to \"In progress\" and turn on \"Move all newly added pull requests here\"")
+                                    note:
+          "Please manage automation for the backlog. Change preset to \"In progress\" and turn" +
+          "on \"Move all newly added pull requests here\"")
       }
       if let doneID = doneID {
-        githubAPI.createProjectCard(cardsURL: DefaultConfigParams.githubBaseURL + "/projects/columns/" + doneID + "/cards",
+        githubAPI.createProjectCard(cardsURL:
+          DefaultConfigParams.githubBaseURL + "/projects/columns/" + doneID + "/cards",
                                     contentID: nil,
                                     contentType: nil,
-                                    note: "Please manage automation for the backlog. Change preset to \"Done\" and turn on \"Move all closed issues here\", \"Move all merged pull requests here\", \"Move all closed, unmerged pull requests here\"")
+                                    note:
+          "Please manage automation for the backlog. Change preset to \"Done\" and turn on" +
+          "\"Move all closed issues here\", \"Move all merged pull requests here\", \"Move all" +
+          "closed, unmerged pull requests here\"")
       }
 
-      // Get last sprint's columns
+      // Get last sprint's columns.
       guard let columnsURL = githubData.project?.columns_url else {
         LogFile.error("couldn't get the columns URL of the previous sprint")
         return
@@ -125,7 +135,7 @@ class ProjectAnalysis {
     }
   }
 
-  class func parseCardContentURL(card: [String: Any]) -> (Int, String)? {
+  private class func parseCardContentURL(card: [String: Any]) -> (Int, String)? {
     var (contentID, contentType): (Int, String)
     guard let contentURL = card["content_url"] as? String else {
       return nil
@@ -146,7 +156,7 @@ class ProjectAnalysis {
     return (contentID, contentType)
   }
 
-  class func createCardFromCard(with card: [String: Any],
+  private class func createCardFromCard(with card: [String: Any],
                                 and columnID: String?,
                                 githubAPI: GithubAPI) {
     if let columnID = columnID {
